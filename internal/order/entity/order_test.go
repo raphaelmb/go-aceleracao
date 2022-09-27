47112016692f0c4db28d78f3c0ff1a297b5a93a9
Raphael Berger <raphaelmb@gmail.com>
2022-09-27 10:30:31 -0300

    Project setup

---
+++ internal/order/entity/order_test.go
@@ -0,0 +1,40 @@
+package entity_test
+
+import (
+	"testing"
+
+	"github.com/raphaelmb/go-aceleracao/internal/order/entity"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGivenAnEmptyId_WhenCreateANewOrder_ThenShouldReceiveAnError(t *testing.T) {
+	order := entity.Order{}
+	assert.Error(t, order.IsValid(), "invalid id")
+}
+
+func TestGivenAnEmptyPrice_WhenCreateANewOrder_ThenShouldReceiveAnError(t *testing.T) {
+	order := entity.Order{ID: "123"}
+	assert.Error(t, order.IsValid(), "invalid price")
+}
+
+func TestGivenAnEmptyTax_WhenCreateANewOrder_ThenShouldReceiveAnError(t *testing.T) {
+	order := entity.Order{ID: "123", Price: 10}
+	assert.Error(t, order.IsValid(), "invalid tax")
+}
+
+func TestGivenValidParams_WhenCallNewOrder_ThenShouldReceiveCreateOrderWithAllParams(t *testing.T) {
+	order, err := entity.NewOrder("123", 10, 2)
+	assert.NoError(t, err)
+	assert.Equal(t, "123", order.ID)
+	assert.Equal(t, 10.0, order.Price)
+	assert.Equal(t, 2.0, order.Tax)
+}
+
+func TestGivenValidParams_WhenCallCalculateFinalPrice_ThenShouldCalculateFinalPriceAndSetItOnFinalPriceProperty(t *testing.T) {
+	order, err := entity.NewOrder("123", 10, 2)
+	assert.NoError(t, err)
+	err = order.CalculateFinalPrice()
+	assert.NoError(t, err)
+	assert.Equal(t, 12.0, order.FinalPrice)
+
+}
