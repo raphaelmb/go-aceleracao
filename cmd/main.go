81d1313109afdcbaa9c14b77aa12dcc2d8168029
Raphael Berger <raphaelmb@gmail.com>
2022-09-29 11:33:43 -0300

    Add rabbitmq + prometheus

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
+	"fmt"
+	"sync"
 
+	amqp "github.com/rabbitmq/amqp091-go"
 	"github.com/raphaelmb/go-aceleracao/internal/order/infra/database"
 	"github.com/raphaelmb/go-aceleracao/internal/order/usecase"
+	rabbitmq "github.com/raphaelmb/go-aceleracao/pkg"
 )
 
 func main() {
+	maxWorkers := 3
+	wg := sync.WaitGroup{}
 	db, err := sql.Open("mysql", "root:root@tcp(mysql:3306)/orders")
 	if err != nil {
 		panic(err)
@@ -17,17 +24,49 @@ func main() {
 	repository := database.NewOrderRepository(db)
 	uc := usecase.NewCalculateFinalPriceUseCase(repository)
 
-	input := usecase.OrderInputDTO{
-		ID:    "1234",
-		Price: 100,
-		Tax:   10,
-	}
-
-	output, err := uc.Execute(input)
+	ch, err := rabbitmq.OpenChannel()
 	if err != nil {
 		panic(err)
 	}
+	defer ch.Close()
 
-	println(output.FinalPrice)
+	out := make(chan amqp.Delivery)
+	go rabbitmq.Consume(ch, out)
 
+	wg.Add(maxWorkers)
+	for i := 0; i < maxWorkers; i++ {
+		defer wg.Done()
+		go worker(out, uc, i)
+	}
+	wg.Wait()
+
+	// input := usecase.OrderInputDTO{
+	// 	ID:    "1234",
+	// 	Price: 100,
+	// 	Tax:   10,
+	// }
+
+	// output, err := uc.Execute(input)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// println(output.FinalPrice)
+
+}
+
+func worker(deliveryMessage <-chan amqp.Delivery, uc *usecase.CalculateFinalPriceUseCase, workerId int) {
+	for msg := range deliveryMessage {
+		var input usecase.OrderInputDTO
+		err := json.Unmarshal(msg.Body, &input)
+		if err != nil {
+			fmt.Println("Error unmarshalling message", err)
+		}
+		input.Tax = 10.0
+		_, err = uc.Execute(input)
+		if err != nil {
+			fmt.Println("Error unmarshalling message", err)
+		}
+		msg.Ack(false)
+	}
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,8 @@ require github.com/stretchr/testify v1.8.0
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-sql-driver/mysql v1.6.0
+	github.com/google/uuid v1.3.0
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rabbitmq/amqp091-go v1.5.0
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
